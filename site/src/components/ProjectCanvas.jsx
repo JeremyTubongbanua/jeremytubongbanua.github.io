14b4f02022ca87799a499922485638537cf250aa
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-12-26 17:12:00 -0500

    feat: sorting

--- site/src/components/ProjectCanvas.jsx
+++ site/src/components/ProjectCanvas.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const ProjectListFilter = ({filter, setFilter}) => {
+const ProjectFilter = ({filter, setFilter}) => {
   return (
     <div className="project-filter">
       <button onClick={() => {
@@ -10,10 +10,37 @@ const ProjectListFilter = ({filter, setFilter}) => {
   );
 }
 
-const ProjectList = ({filteredProjects}) => {
+const ProjectSort = ({sort, setSort}) => {
+  return (
+    <div className="project-sort">
+      <label>
+        Sort By:
+        <select value={sort.by} onChange={(e) => {
+          setSort({...sort, by: e.target.value});
+        }}>
+          <option value="Date">Date</option>
+          <option value="Title">Title</option>
+          <option value="Association">Association</option>
+          <option value="Progress">Progress</option>
+        </select>
+      </label>
+      <label>
+        Order:
+        <select value={sort.order} onChange={(e) => {
+          setSort({...sort, order: e.target.value});
+        }}>
+          <option value="Ascending">Ascending</option>
+          <option value="Descending">Descending</option>
+        </select>
+      </label>
+    </div>
+  );
+}
+
+const ProjectList = ({processedProjects}) => {
   return (
     <ul>
-      {filteredProjects.map((project) => (
+      {processedProjects.map((project) => (
         <li key={project.url}>
           <a href={project.url}>
             {project.frontmatter.title}
@@ -32,8 +59,12 @@ const ProjectCanvas = ({allProjects}) => {
   //   hidden: (true/false)
   // }
   //
- 
-  // All by default
+  // sort: {
+  //  by: 'Date'/'Title'/'Association'/'Progress'
+  //  order: 'Ascending'/'Descending'
+  // }
+
+  // All on by default
   const [filter, setFilter] = useState({
     languages: ['Python', 'C', 'C++', 'Dart', 'Java', 'HTML/CSS/JavaScript', 'No Programming Language'],
     progress: ['In-Progress', 'Finished'],
@@ -41,33 +72,48 @@ const ProjectCanvas = ({allProjects}) => {
     showHidden: false,
   });
 
-  const [filteredProjects, setFilteredProjects] = useState([]);
+  const [sort, setSort] = useState({
+    by: 'Date',
+    order: 'Descending',
+  });
+
+  const [processedProjects, setProcessedProjects] = useState([]);
 
   useEffect(() => {
-    // update projects whenever filter is updated
-    const updatedProjects = allProjects.filter((project) => {
-      // not hidden OR we're showing hidden items
-      const shouldBeShown = !project.frontmatter.hidden || filter.showHidden
-      const matchesLanguages = project.frontmatter.languages.length == 0 ? filter.languages.includes('No Programming Language') : project.frontmatter.languages.some((language) => filter.languages.includes(language));
+    const filteredProjects = allProjects.filter((project) => {
+      const shouldBeShown = !project.frontmatter.hidden || filter.showHidden;
+      const matchesLanguages = project.frontmatter.languages.length === 0 
+        ? filter.languages.includes('No Programming Language') 
+        : project.frontmatter.languages.some((language) => filter.languages.includes(language));
       const matchesProgress = filter.progress.includes(project.frontmatter.progress);
       const matchesAssociation = filter.association.includes(project.frontmatter.association);
 
-      const result = shouldBeShown && matchesLanguages && matchesProgress && matchesAssociation;
-      if(!result) {
-        console.log(`${project.frontmatter.title} 
-          was not included in filtered list 
-          ${matchesLanguages} | ${matchesProgress} | ${matchesAssociation}`);
-      }
-      return result;
+      return shouldBeShown && matchesLanguages && matchesProgress && matchesAssociation;
     });
-    setFilteredProjects(updatedProjects);
-  }, [filter]);
+
+    const sortedProjects = [...filteredProjects].sort((projectA, projectB) => {
+      const multiplier = sort.order === 'Ascending' ? 1 : -1;
+
+      if(sort.by === 'Date') {
+        return multiplier * (new Date(projectA.frontmatter.date) - new Date(projectB.frontmatter.date));
+      } else if(sort.by === 'Title') {
+        return multiplier * projectA.frontmatter.title.localeCompare(projectB.frontmatter.title);
+      } else if(sort.by === 'Association') {
+        return multiplier * projectA.frontmatter.association.localeCompare(projectB.frontmatter.association);
+      } else if(sort.by === 'Progress') {
+        return multiplier * projectA.frontmatter.progress.localeCompare(projectB.frontmatter.progress);
+      }
+    });
+
+    setProcessedProjects(sortedProjects);
+  }, [filter, sort, allProjects]);
 
   return (
     <div>
       <h2>Projects</h2>
-      <ProjectListFilter filter={filter} setFilter={setFilter} />
-      <ProjectList filteredProjects={filteredProjects} />
+      <ProjectFilter filter={filter} setFilter={setFilter} />
+      <ProjectSort sort={sort} setSort={setSort} />
+      <ProjectList processedProjects={processedProjects} />
     </div>
   );
 }
